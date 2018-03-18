fd408b9f1668b9913b6d59384563ca7408c0537d
Mike308 <m.werner9205@gmail.com>
2018-03-18 15:44:01 +0100

    - in method connectToRadio added call of method receivingLoop - added receiving strings

--- nrf24l01worker.cpp
+++ nrf24l01worker.cpp
@@ -1,31 +1,43 @@
 #include "nrf24l01worker.h"
 
-nrf24l01Worker::nrf24l01Worker(uint16_t cePin, uint16_t csPin, uint32_t spiSpeed)
+NRF24L01worker::NRF24L01worker(uint16_t cePin, uint16_t csPin, uint32_t spiSpeed)
 {
     radio = new RF24(cePin,csPin, spiSpeed);
+    this->radioConnected = false;
+
+
 }
 
 
-bool nrf24l01Worker::connectToRadio(uint16_t cePin, uint16_t csPin){
-    
+bool NRF24L01worker::connectToRadio(){
+    qDebug () << "Connecting to radio...";
     radio->begin();
+    radio->printDetails();
+    this->receivingLoop();
+
     
 }
 
-void nrf24l01Worker::selectSendingPipes(const uint8_t *adress){
+void NRF24L01worker::selectSendingPipes(const uint8_t *adress){
     
     radio->openWritingPipe(adress);
 }
 
-void nrf24l01Worker::selectRecevingPipes(int number, const uint8_t *adress){
+void NRF24L01worker::selectRecevingPipes(int number, const uint64_t adress){
     
     radio->openReadingPipe(number, adress);
 }
 
-void nrf24l01Worker::receivingLoop(){
+void NRF24L01worker::receivingLoop(){
 
-    char msg[] = " ";
+
+
+    char msg[] = "";
+    int ReceivedMessage[1] = {000};
     int len = 0;
+    qDebug () << "Start listening...";
+    radio->startListening();
+
 
     while(1){
 
@@ -34,14 +46,21 @@ void nrf24l01Worker::receivingLoop(){
             len = radio->getDynamicPayloadSize();
             radio->read(&msg, len);
             QString stringMsg = QString(msg);
-
+            qDebug () << "Received data: " << stringMsg;
+            emit dataReceived(stringMsg, 0);
+            QThread::sleep(1);
 
 
         }
 
     }
 
+
+
 }
+
+
+
 
 
 
